b27de26fc97ec436f76723b29987408be4b6cd4a
tayfunerbilen <tayfunerbilen@gmail.com>
2018-11-05 00:41:24 +0300

    main files

---
+++ gulpfile.js
@@ -0,0 +1,78 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const minifyCSS = require('gulp-csso');
+const minifyImg = require('gulp-imagemin');
+const minifyJS = require('gulp-uglify');
+const minifyHTML = require('gulp-htmlmin');
+const concat = require('gulp-concat');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const runSequence = require('run-sequence');
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        server: {
+            baseDir: "dist"
+        }
+    });
+    gulp.watch("src/**/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('css', () => {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({
+            outputStyle: 'nested',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', sass.logError))
+        .pipe(minifyCSS())
+        .pipe(autoprefixer())
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('dist/css'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('js', () => {
+    return gulp.src('src/js/**/*.js')
+        .pipe(concat('app.min.js'))
+        .pipe(minifyJS())
+        .pipe(gulp.dest('dist/js'))
+        .pipe(browserSync.stream())
+});
+
+gulp.task('html', () => {
+    gulp.src('src/**/*.html')
+        .pipe(minifyHTML({
+            collapseWhitespace: true,
+            removeComments: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('img', () => {
+    gulp.src('src/img/**/*')
+        .pipe(minifyImg())
+        .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('delete', () => del(['dist/css', 'dist/js', 'dist/img', 'dist/**/*.html']));
+
+gulp.task('watch', () => {
+    gulp.watch("src/scss/**/*.scss", ['css']);
+    gulp.watch("src/js/**/*.js", ['js']);
+    gulp.watch("src/img/**/*", ['img']);
+    gulp.watch("src/**/*.html", ['html']);
+});
+
+gulp.task('default', () => {
+    runSequence(
+        'delete',
+        'html',
+        'css',
+        'js',
+        'img',
+        'browser-sync',
+        'watch'
+    );
+});
