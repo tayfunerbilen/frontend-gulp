337dced9b9e20dec66954010232df4da2344f730
SELAHATTİN KAĞAN CİN <kagann@SELAHATTIN-MacBook-Pro.local>
2019-03-20 15:42:46 +0300

    copied from the node_modules folder under the libs folder
    The libraries loaded with npm were copied from the node_modules folder under the libs folder.Source and output folders assigned to variable

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,21 @@ const concat = require('gulp-concat');
 const autoprefixer = require('gulp-autoprefixer');
 const del = require('del');
 const runSequence = require('run-sequence');
+const npmDist = require('gulp-npm-dist');
+
+const homeDirectory="src"
+const resultDirectory="front"
 
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
-            baseDir: "dist"
+            baseDir: homeDirectory
         }
     });
 });
 
 gulp.task('css', () => {
-    return gulp.src('src/scss/**/*.scss')
+    return gulp.src(resultDirectory+'/scss/**/*.scss')
         .pipe(sass({
             outputStyle: 'nested',
             precision: 10,
@@ -27,37 +31,42 @@ gulp.task('css', () => {
         .pipe(minifyCSS())
         .pipe(autoprefixer())
         .pipe(concat('app.min.css'))
-        .pipe(gulp.dest('dist/css'))
+        .pipe(gulp.dest(homeDirectory+'/css'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('js', () => {
-    return gulp.src('src/js/**/*.js')
+    return gulp.src(resultDirectory+'/js/**/*.js')
         .pipe(concat('app.min.js'))
         .pipe(minifyJS())
-        .pipe(gulp.dest('dist/js'))
+        .pipe(gulp.dest(homeDirectory+'/js'))
         .pipe(browserSync.stream());
 });
 
 gulp.task('html', () => {
-    gulp.src('src/**/*.html')
-        .pipe(gulp.dest('dist'))
+    gulp.src(resultDirectory+'/**/*.html')
+        .pipe(gulp.dest(homeDirectory))
         .pipe(browserSync.stream());
 });
 
 gulp.task('img', () => {
-    gulp.src('src/img/**/*')
+    gulp.src(resultDirectory+'/img/**/*')
         .pipe(minifyImg())
-        .pipe(gulp.dest('dist/img'));
+        .pipe(gulp.dest(homeDirectory+'/img'));
 });
 
-gulp.task('delete', () => del(['dist/css', 'dist/js', 'dist/img', 'dist/**/*.html']));
+gulp.task('copy:libs', function() {
+    gulp.src(npmDist(), {base:'./node_modules'})
+      .pipe(gulp.dest('./'+homeDirectory+'/libs'));
+});
+
+gulp.task('delete', () => del([homeDirectory+'/css', homeDirectory+'/js', homeDirectory+'/img', homeDirectory+'/**/*.html']));
 
 gulp.task('watch', () => {
-    gulp.watch("src/scss/**/*.scss", ['css']);
-    gulp.watch("src/js/**/*.js", ['js']);
-    gulp.watch("src/img/**/*", ['img']);
-    gulp.watch("src/**/*.html", ['html']);
+    gulp.watch(resultDirectory+"/scss/**/*.scss", ['css']);
+    gulp.watch(resultDirectory+"/js/**/*.js", ['js']);
+    gulp.watch(resultDirectory+"/img/**/*", ['img']);
+    gulp.watch(resultDirectory+"/**/*.html", ['html']);
 });
 
 gulp.task('default', () => {
